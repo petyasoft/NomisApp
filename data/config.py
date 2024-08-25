9d2637b4b3eaa53199041facf89eec0c8c82661d
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-08-25 18:03:13 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,47 @@
+# api id, hash
+API_ID = 111111
+API_HASH = 'hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '' # API TOKEN get in @BotFather
+CHAT_ID = '' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [1,300]
+
+# рефка например ref_SpmeVi1tSP
+REFERRAL_CODE  = ""
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# какой часовой пояс на сервере или устройстве +3UTC = 180(минуты)
+UTC = 180
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = False # True/False
+
+# задержка между клеймом и стартом фарминга
+START_SLEEP = [30,60]
+
+# задержка между тасками
+TASKS_SLEEP = [20,40]
+
+# задержка между кругами
+END_SLEEP = [5000,10000]
+
+# мини задержки
+MINI_SLEEP = [1,20] #[min,max]
+
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
